50e3609713c75213f031fcf0f45aaa0e068692b8
Jeb Rosen <jeb@jebrosen.com>
2019-07-27 09:15:23 -0700

    Update helmet in contrib.

--- contrib/lib/src/lib.rs
+++ contrib/lib/src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(crate_visibility_modifier)]
 #![feature(doc_cfg)]
+#![feature(async_await)]
 
 #![doc(html_root_url = "https://api.rocket.rs/v0.5")]
 #![doc(html_favicon_url = "https://rocket.rs/v0.5/images/favicon.ico")]
